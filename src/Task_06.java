193da79a3fc1088d93b941df35e6bf7f1f62d9ee
Stanislaw Tkachenko <stas1696@mail.ru>
2023-11-20 20:14:40 +0400

    add commit Task_06.java

---
+++ src/Task_06.java
@@ -0,0 +1,15 @@
+public class Task_06 {
+    static int COUNT = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Задание №6");
+    }
+
+    static int sum(int number) {
+        int sum = number + 10;
+        return sum;
+
+
+    }
+}
+
